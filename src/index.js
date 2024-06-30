4eb5e22143d32e4cae1d6ee6297b1063daaf5753
root <root@LAPTOP-ADJ5460M>
2024-06-29 18:02:39 -0700

    background

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Starfield from 'react-starfield';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <Starfield
+        starCount={1000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+        /> 
+   <App/>
+    
   </React.StrictMode>
 );
 
