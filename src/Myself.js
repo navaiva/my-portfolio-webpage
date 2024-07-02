228f68b48c078e8d0f094d11ceccb82ee93b6184
root <root@LAPTOP-ADJ5460M>
2024-07-01 19:56:29 -0700

    myself-part

--- src/Myself.js
+++ src/Myself.js
@@ -17,13 +17,17 @@ function App() {
 
     return (
       <div className="whole">
-        <div className="left-s"></div>
+        <div className="left-s">
+          <h1 className="Fh1">Hi, I'm Ivan Navarro</h1>
+          <h1 className="Sh1">Software Engineer</h1>
+          <h1 className="Th1">Welcome to my page!</h1>
+        </div>
         <div className="right-s">
    
         <Canvas camera={camera}>
           <OrbitControls  />
           <Suspense fallback={null}>
-           <Scene position={[10, -70, 0]} />
+           <Scene position={[10, -50, 0]} />
           </Suspense>
         </Canvas>
         </div>
