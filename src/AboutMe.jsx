6f775d55f27170c0feaaea58bdcbd81a7e5dc949
root <root@LAPTOP-ADJ5460M>
2024-07-16 14:46:12 -0700

    projects

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -7,9 +7,8 @@ function App() {
         <div className='about-me'>
             <h1 className='about-1'>About me</h1>
             <h1 className='about'>I am a Full Stack Software Engineer I enjoy work more in the Front-end improving my skills to create creative and interactive UI to improve the experience of the user, but I also enjoy working on the Back-end.</h1>
-            <h1 className='about'>I am from Mexico and I grow up in there, but currently I'm living in Lompoc California. I love Technology, so I research about a career in this field and I get in love with Software Engineer</h1>
-            <h1 className='about'>in my past jobs I do a lot of things I did landscaping, construction, irrigation systems, field jobs, McDonalds, tile installation, cut wood, delivery food </h1>
-            <h1 className='about'>in my free time, I enjoy ride motorcycle, video games, listen to music, hang out with friends, and visit amazing places like forests, beaches, aquariums, museums, etc.</h1>
+            <h1 className='about'>I am from Mexico and I grow up in there, but currently I'm living in Lompoc California. In my past jobs I do a lot of things I did landscaping, construction, irrigation systems, field jobs, McDonalds, tile installation, cut wood, delivery food </h1>
+            <h1 className='about'>In my free time, I enjoy ride motorcycle, video games, listen to music, hang out with friends, and visit amazing places like forests, beaches, aquariums, museums, etc.</h1>
         </div>
       );
  }
