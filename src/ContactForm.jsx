ab51b8a06a61eb109732a6086195d6db0dcc9163
root <root@LAPTOP-ADJ5460M>
2024-07-26 17:48:41 -0700

    deploy

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,62 +1,70 @@
 import React from 'react';
-import './contactForm.css'
-
+import './contactForm.css';
 
 const ContactForm = () => {
-    const [result, setResult] = React.useState("");
+  const [result, setResult] = React.useState('');
 
-    const key = process.env.REACT_APP_EMAIL_KEY
+  const key = process.env.REACT_APP_EMAIL_KEY;
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    setResult("Sending....");
+    setResult('Sending....');
     const formData = new FormData(event.target);
 
-    formData.append("access_key", key);
+    formData.append('access_key', key);
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
     });
 
     const data = await response.json();
 
     if (data.success) {
-      setResult("Form Submitted Successfully");
+      setResult('Form Submitted Successfully');
       event.target.reset();
     } else {
-      console.log("Error", data);
+      console.log('Error', data);
       setResult(data.message);
     }
   };
 
-    return (
-        <form onSubmit={onSubmit}>
-            <div className='formStyle'>
-            <div className='names'>
-            <label className='labels'>Name
-                <input className='inputs' type="text" name="name" required/>
-            </label>
-            </div>
-            <div className='emails'>
-            <label className='labels'>
-                Email
-                <input className='inputs' type="email" name="email" required/>
-            </label>
-            </div>
-            <div className='messages'>
-            <label className='labels'>
-                Message
-                <textarea className='inputs messagesSpace' name="message" required ></textarea>
-            </label>
-            </div>
-            <div className='buttonPosition'>
-            <button  type="submit"  className="button" color={ '#00CCFF'} >
-    Send Message </button>
-            </div>
-            </div>
-        </form>
-    );
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="formStyle">
+        <div className="names">
+          <label className="labels">
+            Name
+            <input className="inputs" type="text" name="name" required />
+          </label>
+        </div>
+        <div className="emails">
+          <label className="labels">
+            Email
+            <input className="inputs" type="email" name="email" required />
+          </label>
+        </div>
+        <div className="messages">
+          <label className="labels">
+            Message
+            <textarea
+              className="inputs messagesSpace"
+              name="message"
+              required
+            ></textarea>
+          </label>
+        </div>
+        <div className="buttonPosition">
+          <button type="submit" className="button" color={'#00CCFF'}>
+            Send Message{' '}
+          </button>
+        </div>
+        <div>
+          <span className="resultMessage">{result}</span>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
